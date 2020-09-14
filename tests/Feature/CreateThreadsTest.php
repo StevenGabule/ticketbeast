9d7c680467cd21b28b885de18c18e4c3476d6f87
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-09-14 22:17:07 +0800

    learn how to test validation errors

--- tests/Feature/CreateThreadsTest.php
+++ tests/Feature/CreateThreadsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Thread;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
@@ -19,8 +20,34 @@ class CreateThreadsTest extends TestCase
     public function testAnAuthenticatedUserCanCreateNewForumThreads()
     {
         $this->signIn();
-        $thread = create('App\Thread');
-        $this->post('/threads', $thread->toArray());
-        $this->get($thread->path())->assertSee($thread->title)->assertSee($thread->body);
+        $thread = make('App\Thread');
+        $response = $this->post('/threads', $thread->toArray());
+        $this->get($response->headers->get('Location'))->assertSee($thread->title)->assertSee($thread->body);
+    }
+
+    function testAThreadRequiresATitle()
+    {
+        $this->publishThread(['title' => null])
+            ->assertSessionHasErrors('title');
+    }
+
+    function testAThreadRequiresABody()
+    {
+        $this->publishThread(['body' => null])
+            ->assertSessionHasErrors('body');
+    }
+
+    function test_a_thread_requires_a_valid_channel()
+    {
+        factory('App\Channel', 2)->create();
+        $this->publishThread(['channel_id' => 999])
+            ->assertSessionHasErrors('channel_id');
+    }
+
+    public function publishThread($overrides = [])
+    {
+        $this->withExceptionHandling()->signIn();
+        $thread = make(Thread::class, $overrides);
+        return $this->post('/threads', $thread->toArray());
     }
 }
--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -25,4 +25,12 @@ class ParticipateInForumTest extends TestCase
         $this->post($thread->path() . '/replies', $reply->toArray());
         $this->get($thread->path())->assertSee($reply->body);
     }
+
+    function test_a_reply_requires_a_body() {
+        $this->withExceptionHandling()->signIn();
+        $thread = create(Thread::class);
+        $reply = make(Reply::class, ['body' => null]);
+        $this->post($thread->path() . '/replies', $reply->toArray())
+                ->assertSessionHasErrors('body');
+    }
 }
