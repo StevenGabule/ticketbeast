f82e69d8dc86174fa19a23c2a2b23bf223b8021d
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-09-15 15:32:23 +0800

    reply details paginate

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -52,7 +52,10 @@ class ThreadsController extends Controller
 
     public function show($channelId, Thread $thread)
     {
-        return view('threads.show', compact('thread'));
+        return view('threads.show', [
+            'thread' => $thread,
+            'replies' => $thread->replies()->paginate(2)
+        ]);
     }
 
     public function edit($id)
--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="container">
-        <div class="row justify-content-center">
+        <div class="row">
             <div class="col-md-8">
                 <div class="card">
                     <div class="card-header">
@@ -13,15 +13,23 @@
                     <div class="card-body">
                         {{$thread->body}}
                     </div>
-                </div>
-            </div>
-        </div>
+                </div><!-- end of card -->
 
-        <div class="row justify-content-center mt-3">
-            <div class="col-md-8">
-                @foreach($thread->replies as $reply)
+                @foreach($replies as $reply)
                     @include('threads.reply')
                 @endforeach
+                {{ $replies->links() }}
+            </div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <p>
+                            This thread was published {{ $thread->created_at->diffForHumans() }} by
+                            <a href="#">{{ $thread->creator->name }}</a>,
+                            and currently has {{ $thread->replies_count }} {{ str_plural('comment', $thread->replies_count) }}
+                        </p>
+                    </div>
+                </div><!-- end of card -->
             </div>
         </div>
 
