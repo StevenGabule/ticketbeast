df13a3df273203073b6082c012bc692df00c47d8
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-09-16 22:21:09 +0800

    implement activity feed with tdd part 2

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,15 +3,28 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
 
 class ProfileController extends Controller
 {
     public function show(User $user)
     {
+        $activities = $this->getActivity($user);
         return view('profiles.show', [
             'profileUser' => $user,
-            'threads' => $user->threads()->paginate(30)
+            'activities' => $activities
         ]);
+    }
+
+    /**
+     * @param User $user
+     * @return Collection
+     */
+    protected function getActivity(User $user): Collection
+    {
+        return $user->activity()->latest()->with('subject')->take(50)->get()->groupBy(function ($activity) {
+            return $activity->created_at->format('Y-m-d');
+        });
     }
 }
--- resources/views/profiles/show.blade.php
+++ resources/views/profiles/show.blade.php
@@ -8,23 +8,12 @@
                 <small>Since {{ $profileUser->created_at->diffForHumans() }}</small>
             </h1>
         </div>
-        @foreach($threads as $thread)
-            <div class="card mt-3">
-                <div class="card-header">
-                    <div class="level">
-                        <span class="flex">
-                            <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a>
-                            Posted:
-                            <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
-                        </span>
-                        <span>{{ $thread->created_at->diffForHumans() }}</span>
-                    </div>
-                </div>
-                <div class="card-body">
-                    {{ $thread->body }}
-                </div>
-            </div>
+        @foreach($activities as $date => $activity)
+            <h3 class="page-header">{{$date}}</h3>
+            @foreach($activity as $record)
+                @include("profiles.activities.{$record->type}", ['activity' => $record])
+            @endforeach
         @endforeach
-        {{$threads->links()}}
+{{--        {{$threads->links()}}--}}
     </div>
 @endsection
