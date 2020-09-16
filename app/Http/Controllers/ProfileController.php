93ff5839f439dc68d0b9a7a6dca174f9b9076a4b
John Paul Lim Gabule <lucas.gabule@gmail.com>
2020-09-16 23:12:48 +0800

    extracting controller queries to the model

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Activity;
 use App\User;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
@@ -10,21 +11,9 @@ class ProfileController extends Controller
 {
     public function show(User $user)
     {
-        $activities = $this->getActivity($user);
         return view('profiles.show', [
             'profileUser' => $user,
-            'activities' => $activities
+            'activities' => Activity::feed($user)
         ]);
-    }
-
-    /**
-     * @param User $user
-     * @return Collection
-     */
-    protected function getActivity(User $user): Collection
-    {
-        return $user->activity()->latest()->with('subject')->take(50)->get()->groupBy(function ($activity) {
-            return $activity->created_at->format('Y-m-d');
-        });
     }
 }
